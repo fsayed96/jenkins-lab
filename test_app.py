3ba98efcded9d2c1417856ef0c5d92132e157f18
fsayed96 <fahadsayed35@gmail.com>
2026-08-18 21:55:36 -0400

    test_app

--- test_app.py
+++ test_app.py
@@ -1,11 +1,11 @@
 from app import say_hello
 
 def test_hello():
-    assert say_hello() == "Hello from Jenkins!"
+    assert say_hello() == "Hello Jenkins!"
     print("Test passed!")
 
-def test_intentinal_failure():
-    assert False, "intentional failure"
+#def test_intentinal_failure():
+#    assert False, "intentional failure"
 
-test_hello()
-test_intentinal_failure()
+#test_hello()
+#test_intentinal_failure()
